5a99ba8e204d57823d422bff2bbd3e1eddf53d25
msuafue034 <xmsuafue034@ieshnosmachado.org>
2025-02-02 21:13:57 +0100

    FC

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll("nav ul li a");
+    const sections = document.querySelectorAll("section");
+
+    function showSection(id) {
+        sections.forEach(section => {
+            section.classList.remove("active");
+        });
+        document.getElementById(id).classList.add("active");
+    }
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            const sectionId = link.getAttribute("href").substring(1);
+            showSection(sectionId);
+
+            navLinks.forEach(nav => nav.classList.remove("active"));
+            link.classList.add("active");
+        });
+    });
+
+    showSection("informacion-personal");  // Mostrar "Sobre mí" al inicio
+});
+
+const toggleTheme = document.getElementById("toggle-theme");
+
+const iconos = document.querySelectorAll(".navbar-icon");
+
+toggleTheme.addEventListener("click", () => {
+    document.body.classList.toggle("modo-oscuro");
+
+    iconos.forEach(icon => {
+        if (document.body.classList.contains("modo-oscuro")) {
+            icon.src = icon.src.replace("-oscuro.png", "-claro.png");
+        } else {
+            icon.src = icon.src.replace("-claro.png", "-oscuro.png");
+        }
+    });
+});
